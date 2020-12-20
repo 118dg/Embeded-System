94b3aa92c904792f87cd74d0293be5fecc5c0153
118dg <54856465+118dg@users.noreply.github.com>
2020-12-20 14:27:24 +0900

    Add files via upload

---
+++ freertos/buttonled_MSP_EXP432P4111_freertos_ccs/Debug/freertos/stream_buffer.d
@@ -0,0 +1,53 @@
+# FIXED
+
+freertos/stream_buffer.obj: ../freertos/stream_buffer.c
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/string.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/FreeRTOS.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/FreeRTOSConfig.h
+freertos/stream_buffer.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+freertos/stream_buffer.obj: C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/posix/freertos/PTLS.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/projdefs.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/portable.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/deprecated_definitions.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/portable/CCS/ARM_CM4F/portmacro.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/mpu_wrappers.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/task.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/list.h
+freertos/stream_buffer.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/stream_buffer.h
+
+../freertos/stream_buffer.c: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/string.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/FreeRTOS.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/FreeRTOSConfig.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/posix/freertos/PTLS.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/projdefs.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/portable.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/deprecated_definitions.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/mpu_wrappers.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/task.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/list.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/stream_buffer.h: 
---
+++ freertos/buttonled_MSP_EXP432P4111_freertos_ccs/Debug/freertos/tasks.d
@@ -0,0 +1,57 @@
+# FIXED
+
+freertos/tasks.obj: ../freertos/tasks.c
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/string.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/FreeRTOS.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/FreeRTOSConfig.h
+freertos/tasks.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+freertos/tasks.obj: C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/posix/freertos/PTLS.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/projdefs.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/portable.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/deprecated_definitions.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/portable/CCS/ARM_CM4F/portmacro.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/mpu_wrappers.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/task.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/list.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/timers.h
+freertos/tasks.obj: C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/stack_macros.h
+
+../freertos/tasks.c: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/string.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/FreeRTOS.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/FreeRTOSConfig.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/posix/freertos/PTLS.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/projdefs.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/portable.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/deprecated_definitions.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/mpu_wrappers.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/task.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/list.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/timers.h: 
+C:/Temp/freertos/buttonled_MSP_EXP432P4111_freertos_ccs/freertos/include/stack_macros.h: 
